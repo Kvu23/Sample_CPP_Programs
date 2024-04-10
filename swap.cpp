8299c377d61a09a6cd3c03a4e09e26b794499112
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2024-04-10 16:52:40 +0000

    Added doxygen generated descriptions

--- swap.cpp
+++ swap.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
-
+/**
+ * @brief this function does swapping of the two numbers
+ * 
+ * @param num1 first number
+ * @param num2 second number
+ */
 void swap(int num1, int num2)
 {
     int temp=0;
@@ -10,7 +15,11 @@ void swap(int num1, int num2)
     num2=temp;
     cout<<"After swapping number1 and number2 is "<<num1<<" "<<num2 <<endl;   
 }
-
+/**
+ * @brief this is the main functions
+ * 
+ * @return int retirn status 
+ */
 int main()
 {
     int number1{123}, number2{234};
